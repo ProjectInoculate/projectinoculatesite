a7549fb5e77772b1a592aa5d95c7618a8849b88b
James <jtmacon36@gmail.com>
2018-10-30 23:20:32 -0400

    login component dummied in and auth method added

---
+++ src/components/Login.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="login-window">
+    <div class="login-window__inner">
+      <input
+        type="text"
+        placeholder="Username"
+        v-model="UserCreate">
+      <input
+        type="password"
+        placeholder="Password"
+        v-model="UserCreate">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PILogin',
+  methods: {
+    UserCreate () {
+      // Some Stuff
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+.login-window {
+  background-color: tomato;
+  padding: 20px 0px;
+  width: 85%;
+  &__inner {
+    width: 85%;
+    margin: 0px auto;
+  }
+}
+</style>
+
